d4fe440ed26263920f0ee4ab82fb8146ac9f4527
Vinny LaPenna <vinnylapenna@users.noreply.github.com>
2016-08-26 12:12:16 -0500

    Refactor WaitController

--- src/main/java/com/credera/vinnylapenna/WaitController.java
+++ src/main/java/com/credera/vinnylapenna/WaitController.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/wait")
 public class WaitController {
 
-  Object lock = new Object();
-
   @GetMapping()
-  public long waitOnLockFiveSeconds() throws InterruptedException {
+  public String waitOnLockFiveSeconds() throws InterruptedException {
     return this.waitOnLock(5000);
   }
 
   @GetMapping("/{millis}")
-  public long waitOnLock(@PathVariable long millis) throws InterruptedException {
+  public String waitOnLock(@PathVariable long millis) throws InterruptedException {
     long start = System.currentTimeMillis();
-    long end;
 
-    synchronized(lock) {
-      Thread.sleep(millis);
-      end = System.currentTimeMillis();
-    }
+    doSomethingInteresting(millis);
+
+    long end = System.currentTimeMillis();
 
     // Note that this time will not necessarily reflect the actual time the browser waits to receive a response
-    return end - start;
+    return "doSomethingInteresting() took " + (end - start) + " milliseconds";
+  }
+
+  private synchronized void doSomethingInteresting(long millis) throws InterruptedException{
+    Thread.sleep(millis);
   }
 
 }
